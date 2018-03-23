8df7a78ed698815c0007d4404bef0068149d7760
Danny Faerch Boensoee <mail@dannyfb.dk>
2018-03-23 14:24:33 +0100

    clean-up code

--- speedtest.py
+++ speedtest.py
@@ -1,25 +1,7 @@
 #!/usr/bin/env python
-
 import os
-import subprocess
-import argparse
-import time
 
-
-speedtest_result = os.popen('speedtest-cli --server 9928 --simple').read().split()
-
-parser = argparse.ArgumentParser(description='Display WLAN signal strength.')
-parser.add_argument(dest='interface', nargs='?', default='wlp4s0',
-                    help='wlan interface (default: wlp4s0)')
-args = parser.parse_args()
-
-cmd = subprocess.Popen('iwconfig %s' % args.interface, shell=True,
-                   stdout=subprocess.PIPE)
-
-for line in cmd.stdout:
-    if 'Link Quality' in line:
-        print line.lstrip(' '),
-    elif 'Not-Associated' in line:
-        print 'No signal'
+while True:
+   speedtest_result = os.popen('speedtest-cli --server 9928 --simple').read().split()
 
 print(speedtest_result)
