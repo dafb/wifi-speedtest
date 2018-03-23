4cf7088a1525515556f77e7c3ea832858de9e49f
Danny Faerch Boensoee <mail@dannyfb.dk>
2018-03-23 14:37:16 +0100

    text

--- speedtest.py
+++ speedtest.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+# run infinity speedtest-cli 
+
 import os
 
 while True:
    speedtest_result = os.popen('speedtest-cli --server 9928 --simple').read().split()
-
-print(speedtest_result)
+   print(speedtest_result)
